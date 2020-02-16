5dd908657e84e52bc97be00c5775e75eda18b33c
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-16 08:14:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,10 +5,10 @@ def reverse_each_word(phrase)
   
   word_array.each_with_index do |word, index|
   
-    letter_array = word.split
+    letter_array = word.split('')
     
     letter_array.each_with_index do |letter, index|
-      new_phrase += letter_array[letter_array.length - index]
+      new_phrase += letter_array[letter_array.length - index - 1]
       
     end
   new_phrase += " "

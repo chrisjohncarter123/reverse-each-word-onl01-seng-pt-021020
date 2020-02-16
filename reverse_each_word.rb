2cfe1da23ed313c6ef1835cc66602176654215ce
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-16 08:06:42 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word(phrase)
+  words = phrase.split(" ")
+  
+  words.each_with_index do |word, index|
+    letters = word.split
+    old_word = word
+    letters.each_with_index do |letter, index|
+      
+    end
+  end
+  
+  puts result
+end
+
+reverse_each_word("Hello")
